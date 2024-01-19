1157585c79e23a13771f30f887c07ea3fc23d480
lhormaluoma <l.hormaluoma@gmail.com>
2024-01-19 20:55:08 +0200

    Added Header

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import ./header.css
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Header() {
+    return (
+        <div className="Header">
+        <header className="Header-header">
+            <h1>CV-2024</h1>
+        </header>
+        </div>
+    );
+    }
+
+export default Header;
